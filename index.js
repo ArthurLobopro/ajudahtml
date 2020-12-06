e24d5dc68cb4e673b7cde7ed86719250b0539e87
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2020-12-06 13:23:15 -0300

    Index

---
+++ index.js
@@ -0,0 +1,37 @@
+var teme=1
+function verifica(){
+	let hora = new Date().getHours
+	if(hora>20){
+	trocatema()
+	}
+}
+
+function tema(){
+	let home=document.getElementById("home")
+	if(teme==1){
+	document.getElementById("T").href="css/tabelaajuda.css"
+	document.getElementById("cabecalho").href="css/cabecalho.css"
+	home.src="home_white.png"
+	}else{
+	document.getElementById("T").href="css/tabelaajudadark.css"
+	document.getElementById("cabecalho").href="css/cabecalho-dark.css"
+	home.src="home_black.png"
+	}
+}
+function trocatema(){
+	if(teme==1){
+		teme=2
+	}else{
+		teme=1
+	}
+	tema()
+}
+function troca_home(num){
+	let home=document.getElementById("home")
+	if(teme==2 && num==1){
+		home.src="home_white.png"
+	}
+	if(teme==2 && num==2){
+		home.src="home_black.png"
+	}
+}
