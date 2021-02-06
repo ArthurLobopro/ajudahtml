614ac540fbbd7845f779e84cbc469fd42435da4b
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-02-05 21:17:41 -0300

    Animação corrigida

--- help-css/tabela-css.js
+++ help-css/tabela-css.js
@@ -3,3 +3,12 @@ function mostra_esconde(object1,object2) {
     $(object2).toggleClass('no-rotated')
     $(object2).toggleClass('rotated')
 }
+
+document.body.onload = ()=>{
+    const delay = document.querySelectorAll('.no-rotated')
+    setTimeout(() => {
+        for(i in delay){
+            delay[i].classList.remove('delay-1')
+        }
+    }, 2000)
+}
