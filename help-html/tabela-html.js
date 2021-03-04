e07b53b18a2d750d3ccb1fb29b6cb374dca9d84d
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-03-03 21:53:25 -0300

    Tabela html mantendo tema

--- help-html/tabela-html.js
+++ help-html/tabela-html.js
@@ -1,4 +1,4 @@
-var teme=1
+let tema = "white"
 function verifica(){
 	let hora = new Date().getHours
 	if(hora>20){
@@ -6,30 +6,53 @@ function verifica(){
 	}
 }
 
-function tema(){
-	let home=document.getElementById("home")
-	if(teme==1){
-	document.getElementById("cores").href="css/tabela-html-white.css"
-	document.getElementById("cabecalho").href="../css/cabecalho-white.css"
-	}else{
-	document.getElementById("cores").href="css/tabela-html-dark.css"
-	document.getElementById("cabecalho").href="../css/cabecalho-dark.css"
-	}
+const troca_tema = () => {
+	tema = tema == "white" ? "dark" : "white"
+	set_tema()
 }
-function trocatema(){
-	if(teme==1){
-		teme=2
+const set_tema = () => {
+	if(tema=="white"){
+		document.getElementById("cores").href="css/tabela-html-white.css"
+		document.getElementById("cabecalho").href="../css/cabecalho-white.css"
 	}else{
-		teme=1
+		document.getElementById("cores").href="css/tabela-html-dark.css"
+		document.getElementById("cabecalho").href="../css/cabecalho-dark.css"
 	}
-	tema()
+	criaTema()
 }
 function troca(){
 	const circle = document.querySelector('.circle')
-	circle.classList.toggle('left')
-	circle.classList.toggle('right')
-	trocatema()
+	if(tema == "white"){
+		circle.classList.toggle('left')
+		circle.classList.toggle('right')
+	}else{
+		circle.classList.toggle('right')
+		circle.classList.toggle('left')
+	}
+	troca_tema()
 }
 setTimeout(() => {
 	document.querySelector('.circle').classList.remove('ad-2s')
 }, 1000);
+function lerTema(){
+	if(localStorage.tema == undefined){
+		criaTema()
+	}else{
+		tema = localStorage.tema
+	}
+	set_tema()
+	btn_class()
+}
+function criaTema(){
+	localStorage.tema = tema
+}
+const classe = {
+	white: "left", dark: "right"
+}
+const btn_class = () => {
+	const circle = document.querySelector(".circle")
+	circle.classList.add(classe[tema])
+}
+document.addEventListener("DOMContentLoaded",() => {  
+	lerTema() 
+}, false)
