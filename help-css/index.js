ecc019b309dbf3ae6ac0d99279efd995d16a5a39
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-03-11 23:37:30 -0300

    mudanças de index

---
+++ help-css/index.js
@@ -0,0 +1,28 @@
+const get = id => document.getElementById(id)
+const querySelector = str => document.querySelector(str)
+// function mostra_esconde(object1,object2) {
+//     $(object1).toggle(500)
+//     $(object2).toggleClass('no-rotated')
+//     $(object2).toggleClass('rotated')
+// }
+
+// document.body.onload = ()=>{
+//     const delay = document.querySelectorAll('.no-rotated')
+//     setTimeout(() => {
+//         for(i in delay){
+//             delay[i].classList.remove('delay-1')
+//         }
+//     }, 2000)
+// }
+function expandAndMinimize(event){
+    let target = event.target.tagName !== 'IMG' ? event.target : event.target.parentElement
+    target.children[0].classList.toggle('no-rotated')
+    target.children[0].classList.toggle('rotated')
+
+    const element = querySelector(`.${target.dataset.target}`)
+    console.log(element);
+    const display = element.style.display
+    element.style.display = display == 'none' ? '' : 'none'
+}
+const titles = document.getElementsByTagName('h2')
+for(let e of titles){ e.onclick = expandAndMinimize }
